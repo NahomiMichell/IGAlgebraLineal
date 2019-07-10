6dfaa385f8466b83bb1fa75965ec1d90ab6e03d1
Nahomi <nahomi.martinez@ujcv.edu.hn>
2019-07-09 18:13:56 -0600

    Avances

--- src/edu/ujcv/progra2/Nahomi/MenuVectores.java
+++ src/edu/ujcv/progra2/Nahomi/MenuVectores.java
@@ -4,7 +4,52 @@ import edu.ujcv.progra2.Nahomi.MenuDimVec;
 import edu.ujcv.progra2.utility.LectorDeTecladoValidado;
 
 public class MenuVectores {
+    
 public int opc2;
+
 MenuDimVec MDV = new MenuDimVec();
     LectorDeTecladoValidado NM = LectorDeTecladoValidado.getInstance();
+
+    public void presentarOpciones(){
+
+        System.out.println("\t \t Vectores");
+        System.out.println("\n \t Dimensiones \n");
+        System.out.println("\n 1. Vector en R2");
+        System.out.println("\n 2. Vector en R3");
+        System.out.println("\n 3.Vector en R4");
+        System.out.println("\n 4. Regresar al menu principal");
+
+    }
+
+    public int leerOpciones() {
+        opc2 = NM.getInteger("Ingrese una opcion", "Ha ingresado una opcion erronea");
+        return opc2;
+    }
+
+    public int procesarOpcion(int opc2) {
+        switch (opc2) {
+
+            case 1:
+                MDV.mostrarOpcionR2();
+                MDV.operandoR2(MDV.LeeropcionR2());
+                break;
+
+            case 2:
+                MDV.mostrarOpcionR3();
+                MDV.operandoR3(MDV.LeeropcionR3());
+                break;
+
+            case 3:
+                MDV.mostrarOpcionR4();
+                MDV.operandoR4(MDV.LeeropcionR4());
+                break;
+
+            case 4:
+                System.out.println("Has regresado al menu principal \n \n");
+                break;
+
+        }
+        return opc2;
+    }
+
 }
